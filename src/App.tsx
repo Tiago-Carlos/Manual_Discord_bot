b46c5004b2d9838e8466a4d896cb8eddf4273dab
Tiago Carlos <tiagozero3@gmail.com>
2022-10-12 23:58:27 -0300

    corrigido erro de conflito

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,25 @@ function App() {
   const [link_avatar, setLink_avatar] = useState('');
   const [msg, setMsg] = useState('');
   const [username, setUsername] = useState('');
+  const [files, setFiles] = useState<any>();
   
   async function enviar_mensagem() {
-    let obj = await (await fetch(link_webhook)).json();
+    const form = new FormData();
+    let cnt = 0;
+    for (var i = 0; i < files.length; i++) {
+      form.append(`file${i}`, files[i], files[i].name);
+    }
 
-    const httpClient = axios.create({ baseURL: `https://discord.com/api/webhooks`});
-    
-    let msg_json = {
+    form.append('payload_json', JSON.stringify({
       "content": msg,
       "username": username,
       "avatar_url": link_avatar
-    }
-    
-    console.log(`/${obj.id}/${obj.token}`)
+    }))
+    let obj = await (await fetch(link_webhook)).json();
 
-    httpClient.post(`/${obj.id}/${obj.token}`, msg_json);
+    const httpClient = axios.create({ baseURL: `https://discord.com/api/webhooks`});
 
-    console.log(obj.id)
-    console.log(obj.token)
+    httpClient.post(`/${obj.id}/${obj.token}`, form);
   }
   
   return (
@@ -78,21 +79,7 @@ function App() {
                 id="Mensagem" />
           </td>
         </tr>
-
-<<<<<<< Updated upstream
-        <label>{'Mensagem: '}</label>
-        <input type="text"
-            onChange= {e => setMsg(e.target.value)}
-            className="form-control"
-            id="Mensagem"
-            placeholder='Digite a mensagem' />
-        <br/>
-
-        <button onClick={enviar_mensagem}
-            className='btn btn-danger'>
-            <i className='pi pi-plus' /> Enviar mensagem
-        </button>
-=======
+        
         <tr>
           <td colSpan={2}>
             <input type="file" multiple
@@ -112,7 +99,6 @@ function App() {
         </tr>
         </tbody>
       </table>
->>>>>>> Stashed changes
     </div>
   )
 }
